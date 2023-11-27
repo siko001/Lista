f5022a195a09e470c282e554c0fcb80d72a28460
Neil <neil.mallia1@gmail.com>
2023-11-27 18:13:33 +0100

    another update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\ShoppingList;
-use Laravel\Passport\HasApiTokens;
+
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
